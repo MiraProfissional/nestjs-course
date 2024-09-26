3df0a5b8cae53c5e7e8deff9adb8528c164bcd1c
MiraProfissional <jvitor.gmira@gmail.com>
2024-09-26 14:34:27 -0300

    :sparkles: feat: creating CreateUser DTO

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,36 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  MinLength,
+  MaxLength,
+  IsEmail,
+  Matches,
+} from 'class-validator';
+
+export class CreateUserDTO {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(96)
+  firstName: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  @MaxLength(96)
+  lastName?: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
+    message:
+      'Minimum eight characters, at least one letter, one number and one special character',
+  })
+  password: string;
+}
