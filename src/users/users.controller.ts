e4cceef4665541c1165c677ca4ae299494b07bdb
MiraProfissional <jvitor.gmira@gmail.com>
2024-09-26 15:42:50 -0300

    :sparkles: feat: using Mapped Types to avoid code duplication

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,9 +7,11 @@ import {
   Body,
   ParseIntPipe,
   DefaultValuePipe,
+  Patch,
 } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { GetUsersParamDto } from './dtos/get-users-param.dto';
+import { PatchUserDto } from './dtos/patch-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -20,6 +22,8 @@ export class UsersController {
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
     console.log(getUsersParamDto);
+    console.log(limit);
+    console.log(page);
 
     return 'You sent a GET request to users endpoint';
   }
@@ -28,5 +32,11 @@ export class UsersController {
   public createUsers(@Body() createUserDto: CreateUserDTO) {
     console.log(createUserDto instanceof CreateUserDTO);
     return 'You sent a POST request to users endpoint';
+  }
+
+  @Patch()
+  public patchUser(@Body() patchUserDto: PatchUserDto) {
+    console.log(patchUserDto);
+    return 'You sent a PATCH request to users endpoint';
   }
 }
