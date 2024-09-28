685546bb36865f82a525aa9d0d5b7e7932ac2a8d
MiraProfissional <jvitor.gmira@gmail.com>
2024-09-28 17:00:08 -0300

    :sparkles: feat: creating circular dependency with 'Users' and 'Auth'

--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, forwardRef, Inject } from '@nestjs/common';
+import { UsersService } from 'src/users/providers/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject(forwardRef(() => UsersService))
+    private readonly usersService: UsersService,
+  ) {}
+
+  public login(id: string, email: string, password: string) {
+    const user = this.usersService.findOneById('123');
+    return 'SAMPLE_TOKEN';
+  }
+
+  public isAuth() {
+    return true;
+  }
+}
