d3995902f767d365332f6b1382bdfcd17a3bcaa4
MiraProfissional <jvitor.gmira@gmail.com>
2024-10-08 19:53:32 -0300

    :sparkles: feat: documenting GET route for 'Users'

--- src/users/dtos/get-users-param.dto.ts
+++ src/users/dtos/get-users-param.dto.ts
@@ -1,7 +1,12 @@
 import { IsInt, IsOptional } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class GetUsersParamDto {
+  @ApiPropertyOptional({
+    description: 'Get user with a specific id',
+    example: 1234,
+  })
   @IsOptional()
   @IsInt()
   @Type(() => Number) //This was used because the URL is a string, so we need to transform the param into a number
