cc93923d33721e3f1f23c49fd2c8b77780fd3588
MiraProfissional <jvitor.gmira@gmail.com>
2024-10-12 11:35:04 -0300

    :sparkles: feat: compodoc coverage and JSDocs

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -2,13 +2,16 @@ import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
 import { AuthService } from 'src/auth/providers/auth.service';
 
+/** Class to connect to Users table and perform business operations */
 @Injectable()
 export class UsersService {
+  /** The constructor to connect AuthService with UsersService by Dependency Injection*/
   constructor(
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
   ) {}
 
+  /** The method to get all the users from the database */
   public findAll(
     getUsersParamDto: GetUsersParamDto,
     limit: number,
@@ -29,6 +32,7 @@ export class UsersService {
     ];
   }
 
+  /** The method to get one specific user by your ID from the database */
   public findOneById(id: string) {
     return {
       id: 123,
