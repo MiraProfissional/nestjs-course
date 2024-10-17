a229138aed34455ed88de8a6e345d2d9f9137fdd
MiraProfissional <jvitor.gmira@gmail.com>
2024-10-16 23:36:08 -0300

    :sparkles: feat: adding psql to PATH

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,29 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      // Default options
+      type: 'postgres',
+      entities: [],
+
+      // Only in development mode
+      synchronize: true,
+
+      // Postrgres options
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      host: 'localhost',
+      database: 'nestjs-blog',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
