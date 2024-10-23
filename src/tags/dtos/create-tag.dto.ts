b6898fe371556481609730bc50e6f94d0abb7b45
MiraProfissional <jvitor.gmira@gmail.com>
2024-10-23 15:18:03 -0300

    :sparkles: feat: updating DTO files

---
+++ src/tags/dtos/create-tag.dto.ts
@@ -0,0 +1,50 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+  Matches,
+  IsOptional,
+  IsJSON,
+  IsUrl,
+} from 'class-validator';
+
+export class CreateTagDto {
+  @ApiProperty()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(256)
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    description:
+      "A slug shoud be all small letters and uses only '-' and without spaces. For example 'my-url'",
+    example: 'my-blog-post',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(256)
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+    message:
+      'A slug shoud be all small letters and uses only "-" and without spaces. For example "my-url"',
+  })
+  slug: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsJSON()
+  schema?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsUrl()
+  @MaxLength(1024)
+  featuredImageUrl?: string;
+}
