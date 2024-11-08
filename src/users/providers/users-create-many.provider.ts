b3fbb74d4c5c25c62806c8c3845b3d3a20dd0533
MiraProfissional <jvitor.gmira@gmail.com>
2024-11-08 14:44:22 -0300

    :sparkles: feat: creating handle exceptions for CreateManyUsers

--- src/users/providers/users-create-many.provider.ts
+++ src/users/providers/users-create-many.provider.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from '../dtos/create-user.dto';
+import {
+  Injectable,
+  RequestTimeoutException,
+  ConflictException,
+} from '@nestjs/common';
 import { User } from '../user.entity';
 import { DataSource } from 'typeorm';
 import { CreateManyUsersDto } from '../dtos/create-many-users.dto';
@@ -17,27 +20,44 @@ export class UsersCreateManyProvider {
     // 1. Create Query Runner Instance
     const queryRunner = this.dataSource.createQueryRunner();
 
-    // 2. Connect Query Runner to datasource
-    await queryRunner.connect();
+    try {
+      // 2. Connect Query Runner to datasource
+      await queryRunner.connect();
 
-    // 3. Start Transaction
-    await queryRunner.startTransaction();
+      // 3. Start Transaction
+      await queryRunner.startTransaction();
+    } catch (error) {
+      throw new RequestTimeoutException(
+        'Unbale to process your request at the moment, please try later.',
+        {
+          description: `Error connecting to the database. ${String(error)}`,
+        },
+      );
+    }
 
     try {
       for (const user of createManyUsersDto.users) {
         const newUser = queryRunner.manager.create(User, user);
         const result = await queryRunner.manager.save(newUser);
         newUsers.push(result);
-
-        // 4.1. If successful commit
-        await queryRunner.commitTransaction();
       }
+      // 4.1. If successful commit
+      await queryRunner.commitTransaction();
     } catch (error) {
       // 4.2. If unsuccessful rollback
       await queryRunner.rollbackTransaction();
+      throw new ConflictException('Could not complete the transaction', {
+        description: String(error),
+      });
     } finally {
-      // 5. Release connection
-      await queryRunner.release();
+      try {
+        // 5. Release connection
+        await queryRunner.release();
+      } catch (error) {
+        throw new RequestTimeoutException('Could not release the connection', {
+          description: String(error),
+        });
+      }
     }
 
     return newUsers;
