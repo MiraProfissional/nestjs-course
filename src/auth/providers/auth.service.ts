ce7580586c86f1174efd2d78ea4d46f3ac8ed28a
MiraProfissional <jvitor.gmira@gmail.com>
2024-11-16 16:54:38 -0300

    :sparkles: feat: creating refresh tokens endpoint

--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable, forwardRef, Inject } from '@nestjs/common';
 import { UsersService } from 'src/users/providers/users.service';
 import { SignInDto } from '../dtos/signin.dto';
 import { SignInProvider } from './sign-in.provider';
+import { RefreshTokenDto } from '../dtos/refresh-token.dto';
+import { RefreshTokensProvider } from './refresh-tokens.provider';
 
 @Injectable()
 export class AuthService {
@@ -11,13 +13,16 @@ export class AuthService {
 
     //Inject signInProvider
     private readonly signInProvider: SignInProvider,
+
+    //Inject refreshTokenProvider
+    private readonly refreshTokenProvider: RefreshTokensProvider,
   ) {}
 
   public async signIn(signInDto: SignInDto) {
     return await this.signInProvider.signIn(signInDto);
   }
 
-  public isAuth() {
-    return true;
+  public async refreshToken(refreshTokenDto: RefreshTokenDto) {
+    return await this.refreshTokenProvider.refreshToken(refreshTokenDto);
   }
 }
