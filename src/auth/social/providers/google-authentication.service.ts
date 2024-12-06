e742e648b7ade791e85473fc09d65bb1b6385c8b
MiraProfissional <jvitor.gmira@gmail.com>
2024-12-06 13:37:29 -0300

    :sparkles: feat: implement Authentication With Google Token

--- src/auth/social/providers/google-authentication.service.ts
+++ src/auth/social/providers/google-authentication.service.ts
@@ -1,16 +1,23 @@
-import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { forwardRef, Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { OAuth2Client } from 'google-auth-library';
 import jwtConfig from 'src/auth/config/jwt.config';
 import { GoogleTokenDto } from '../dtos/google-token.dto';
+import { UsersService } from 'src/users/providers/users.service';
+import { GenerateTokensProvider } from 'src/auth/providers/generate-tokens.provider';
 
 @Injectable()
 export class GoogleAuthenticationService implements OnModuleInit {
   private oauthClient: OAuth2Client;
 
   constructor(
+    private readonly generateTokensProvider: GenerateTokensProvider,
+
     @Inject(jwtConfig.KEY)
     private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+
+    @Inject(forwardRef(() => UsersService))
+    private readonly usersService: UsersService,
   ) {}
 
   onModuleInit() {
@@ -19,11 +26,23 @@ export class GoogleAuthenticationService implements OnModuleInit {
     this.oauthClient = new OAuth2Client(clientId, clientSecret);
   }
 
-  public async authentication(googleToken: GoogleTokenDto) {
+  public async authenticate(googleTokenDto: GoogleTokenDto) {
     // verify the Google Token sent by User
+    const loginTicket = await this.oauthClient.verifyIdToken({
+      idToken: googleTokenDto.token,
+    });
+
     // extract the payload from Google JWT
+    const { email, sub: googleId } = loginTicket.getPayload();
+
     // find the user in the database using GoogleId
+    const user = await this.usersService.findOneByGoogleId(googleId);
+
     // if googleId exists generate token
+    if (user) {
+      return this.generateTokensProvider.generateTokens(user);
+    }
+
     // if not, create a new user and then generate tokens
     // throw Unauthorised exception
   }
