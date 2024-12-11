4d8671ae6d61a7ab01ed4c10ee555c6a17cb115a
MiraProfissional <jvitor.gmira@gmail.com>
2024-12-11 17:00:16 -0300

    :sparkles: feat: uploads module and configuration

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { config } from 'aws-sdk';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,7 +23,7 @@ async function bootstrap() {
   Swagger configuration
   */
 
-  const config = new DocumentBuilder()
+  const swaggerconfig = new DocumentBuilder()
     .setTitle('NestJs Masterclass - Blog app API')
     .setDescription('Use the base API URL as http://localhost:8000')
     .setTermsOfService('http://localhost:8000/terms-of-service')
@@ -34,8 +36,18 @@ async function bootstrap() {
     .build();
 
   // Instantiate Document
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerconfig);
   SwaggerModule.setup('api', app, document);
+
+  // Setup the aws sdk used uploading the files to aws s3 bucket
+  const configService = app.get(ConfigService);
+  config.update({
+    credentials: {
+      accessKeyId: configService.get('appConfig.awsAcessKeyId'),
+      secretAccessKey: configService.get('awsSecretAccessKey'),
+    },
+    region: configService.get('appConfig.awsRegion'),
+  });
 
   app.enableCors();
 
