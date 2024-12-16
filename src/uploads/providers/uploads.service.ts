6954d58d38c232b6100dda630aecfda04619bb05
MiraProfissional <jvitor.gmira@gmail.com>
2024-12-16 20:59:41 -0300

    :sparkles: feat: complete uploads service

--- src/uploads/providers/uploads.service.ts
+++ src/uploads/providers/uploads.service.ts
@@ -1,9 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Upload } from '../upload.entity';
+import { SaveLocalFilesProvider } from './save-local-files.provider';
+import { UploadToAwsProvider } from './upload-to-aws.provider';
+import { ConfigService } from '@nestjs/config';
+import { UploadFile } from '../interfaces/upload-file.interface';
+import { fileTypes } from '../enums/file-types.enum';
 
 @Injectable()
 export class UploadsService {
+  constructor(
+    private readonly configService: ConfigService,
+
+    private readonly saveLocalFilesProvider: SaveLocalFilesProvider,
+
+    @InjectRepository(Upload)
+    private readonly uploadsRepository: Repository<Upload>,
+
+    private readonly uploadToAwsProvider: UploadToAwsProvider,
+  ) {}
+
   public async uploadFile(file: Express.Multer.File) {
-    // Upload the file to AWS S3
-    // Generate to a new entry in database
+    // Throw error for unsupported MIME types
+    if (
+      ['image/gif', 'image/jpeg', 'image/jpg', 'image/png'].includes(
+        file.mimetype,
+      )
+    ) {
+      throw new BadRequestException('Mime type not supported');
+    }
+
+    try {
+      // Upload the file to AWS S3
+      // const name = await this.uploadToAwsProvider.fileUpload(file);
+
+      // In that case, we will save locally
+      const name = await this.saveLocalFilesProvider.fileUpload(file);
+
+      // Generate to a new entry in database
+      const uploadFile: UploadFile = {
+        name: name,
+        path: `photos/${name}`,
+        type: fileTypes.IMAGE,
+        mime: file.mimetype,
+        size: file.size,
+      };
+
+      const upload = this.uploadsRepository.create(uploadFile);
+
+      return await this.uploadsRepository.save(upload);
+    } catch (error) {
+      throw new ConflictException(error);
+    }
   }
 }
--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { UploadsController } from './uploads.controller';
 import { UploadsService } from './providers/uploads.service';
 import { UploadToAwsProvider } from './providers/upload-to-aws.provider';
+import { SaveLocalFilesProvider } from './providers/save-local-files.provider';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Upload } from './upload.entity';
 
 @Module({
   controllers: [UploadsController],
-  providers: [UploadsService, UploadToAwsProvider]
+  providers: [UploadsService, UploadToAwsProvider, SaveLocalFilesProvider],
+  imports: [TypeOrmModule.forFeature([Upload])],
 })
 export class UploadsModule {}
