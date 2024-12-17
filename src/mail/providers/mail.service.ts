35ccebdc2fe88ced9de72216ef3bcf7c6162112c
MiraProfissional <jvitor.gmira@gmail.com>
2024-12-17 13:56:41 -0300

    :sparkles: feat: testing email service

--- src/mail/providers/mail.service.ts
+++ src/mail/providers/mail.service.ts
@@ -1,4 +1,22 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  constructor(private mailerSerivce: MailerService) {}
+
+  public async sendUserWelcome(user: User): Promise<void> {
+    await this.mailerSerivce.sendMail({
+      to: user.email,
+      from: `Onboarding Team <support@nestjs-blog.com>`,
+      subject: 'Welcome to Nestjs Blog',
+      template: './welcome',
+      context: {
+        name: user.firstName,
+        email: user.email,
+        loginUrl: 'http://localhost:8000',
+      },
+    });
+  }
+}
