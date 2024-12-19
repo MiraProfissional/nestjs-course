03784fdb71ebb0dfbffe059ac868165427d43f65
MiraProfissional <jvitor.gmira@gmail.com>
2024-12-19 19:12:49 -0300

    :sparkles: feat: enabling inline css

--- src/mail/providers/mail.service.ts
+++ src/mail/providers/mail.service.ts
@@ -1,5 +1,6 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import path from 'path';
 import { User } from 'src/users/user.entity';
 
 @Injectable()
@@ -11,19 +12,12 @@ export class MailService {
       to: user.email,
       from: `Onboarding Team <support@nestjs-blog.com>`,
       subject: 'Welcome to Nestjs Blog',
-      html: `
-        <h1>Welcome, ${user.firstName}!</h1>
-        <p>Thank you for signing up for our platform.</p>
-        <p>Your account details:</p>
-        <ul>
-          <li><strong>Email:</strong> ${user.email}</li>
-          <li><strong>Login URL:</strong> 
-            <a href="http://localhost:8000">Click here to log in</a>
-          </li>
-        </ul>
-        <p>Thanks!</p>
-        <p><strong>NestJS Blog Team</strong></p>
-      `,
+      template: path.join(__dirname, '..'),
+      context: {
+        name: user.firstName,
+        email: user.email,
+        loginUrl: 'http://localhost:8000',
+      },
     });
   }
 }
